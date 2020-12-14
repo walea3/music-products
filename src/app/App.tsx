5a128d4b0bad675375f799262b949cd0b093079d
Wale Adeyemi <wale@ade3.co.uk>
2020-12-14 21:33:31 +0000

    chore: add router & page routes
    - replace App content with router & placeholder routes
    - added react router types
    - removed unused App.css

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from 'assets/logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/product-:productId">
+          <div>Product detail</div>
+        </Route>
+        <Route path="/">
+          <div>Products listing</div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
